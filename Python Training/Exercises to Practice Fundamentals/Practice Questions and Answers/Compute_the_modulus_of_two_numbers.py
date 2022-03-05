55e6225483dbb979dfca95e0cbcf0be866727704
dominion314 <dominickhrndz314@mgail.com>
2022-03-04 19:22:45 -0600

    Terraform

--- Python Training/Exercises to Practice Fundamentals/Practice Questions and Answers/Compute_the_modulus_of_two_numbers.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def getModulus(a, b):
-   num1 = getModulus(100)
-   num2 = getModulus(4)
- #--answer --
-   print(num1 % num2)
--- Python Training/Exercises to Practice Fundamentals/Practice Questions and Answers/ch8_2decmial.py
+++
@@ -1,7 +0,0 @@
-'''Print air_temperature with 1 decimal point followed by C. Sample output from given program:
-36.4C
-
-'''
-
-air_temperature = 36.4158102
-print('%0.1fC'%air_temperature)
